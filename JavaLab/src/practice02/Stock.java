19bbb979c9d99dac929d37dc84283fed555c1b22
Jin Gwangchae <neymarbrazil1992@gmail.com>
2026-07-31 18:00:57 +0900

    0731 final

---
+++ JavaLab/src/practice02/Stock.java
@@ -0,0 +1,35 @@
+package practice02;
+
+public class Stock {
+    
+    private String stockName;
+    private int quantity;
+    private int avgPrice;
+    
+    public Stock(){
+
+    }
+    
+    public String getStockName() {
+        return stockName;
+    }
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public int getAvgPrice() {
+        return avgPrice;
+    }
+    public void setAvgPrice(int avgPrice) {
+        this.avgPrice = avgPrice;
+    }
+
+    
+
+
+}//class
